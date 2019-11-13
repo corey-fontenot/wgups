4c64e5fdf3bf13c944dda9c4adb83a9d8595df4b
Corey Fontenot <corey.fontenot@gmail.com>
2019-11-13 10:59:15 -0600

    Run simulation

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 
 import csv
 from configparser import ConfigParser
+
+from data_structures.graph import Graph
+from data_structures.hashtable import HashTable
+from data_structures.queue import Queue
 from wgups.clock import Clock
 from wgups.location import Location
 from wgups.package import Package
 from wgups.truck import Truck
-from data_structures.hashtable import HashTable
-from data_structures.graph import Graph
-from data_structures.queue import Queue
+from wgups.simulation import Simulation
+
 
 packages = HashTable(120)
 locations = Graph()
@@ -26,6 +29,7 @@ NUM_TRUCKS = int(parser.get("trucks", "num_trucks"))
 NUM_DRIVERS = int(parser.get("trucks", "num_drivers"))
 PACKAGES_PER_TRUCK = int(parser.get("trucks", "packages_per_truck"))
 TRUCK_MPH = int(parser.get("trucks", "truck_mph"))
+DELAYED_FLIGHT_TIME = parser.get("application", "delayed_flight_time")
 
 # Read package data from file
 with open(PACKAGE_FILE, 'r') as f:
@@ -58,7 +62,7 @@ with open(DISTANCE_TABLE, 'r') as f:
         for index, distance in enumerate(row[5:]):
             vertex_a = locations.get_vertex(location.name)
             vertex_b = locations.get_vertex(temp_locations[index].name)
-            locations.add_undirected_edge(vertex_a , vertex_b, float(distance))
+            locations.add_undirected_edge(vertex_a, vertex_b, float(distance))
 
 # Create trucks
 # Added in the order they will leave
@@ -71,6 +75,11 @@ truck_queue = Queue()
 for truck in Truck.sort_into_trucks([x for x in packages], truck_list, START_OF_DAY, END_OF_DAY):
     # Add location data for packages in truck
     truck.set_locations(locations)
+    truck.find_route()
 
     # Add truck to truck queue
     truck_queue.push(truck)
+
+# Start simulation
+simulation = Simulation(START_OF_DAY, packages, truck_queue, DELAYED_FLIGHT_TIME)
+simulation.start_simulation()
